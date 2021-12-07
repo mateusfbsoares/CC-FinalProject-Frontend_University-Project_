654faa8910cd54d3961e937e4295e13b58ee2e8f
mfbs <mateusfbsoares@gmail.com>
2021-12-07 10:18:32 -0300

    updatae meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import "tailwindcss/tailwind.css";
 function MyApp({ Component, pageProps }) {
   return (
     <div className="w-full">
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+
       <Component {...pageProps} />
     </div>
   );
